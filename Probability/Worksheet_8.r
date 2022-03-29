50759274ace49296992398ae007dc0784f89d7b7
EvnFox <78891786+EvnFox@users.noreply.github.com>
2022-03-29 16:33:05 -0400

    updte

---
+++ Probability/Worksheet_8.r
@@ -0,0 +1,20 @@
+pbinom(2, size=20, prob= 1/6)
+
+
+a = dbinom(0, size=20, prob =1/6)
+b = dbinom(1, size=20, prob =1/6)
+c = dbinom(2, size=20, prob =1/6)
+a + b + c
+
+
+dbinom(0, size=20, prob =1/6)
+
+
+
+pbinom(1, size=300, prob = 0.02)
+
+ppois(3, 6)
+
+1 - ppois(6, 1.8)
+
+1 - ppois(0, 1.2)
